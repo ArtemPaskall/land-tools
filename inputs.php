cccf17901e8175402f303d287bfa59132349b1fe
Artem <96254791+ArtemPaskall@users.noreply.github.com>
2025-10-02 11:38:42 +0300

    Update and rename inputs.php to white AIO

--- inputs.php
+++
@@ -1,9 +0,0 @@
-<input type="hidden" name="sub3" value="<?=$_GET['sub3'];?>"/>
-<input type="hidden" name="sub2" value="<?=$_GET['sub2'];?>"/>
-<input type="hidden" name="pixel" value="<?=$_GET['pixel'];?>"/>
-
-Передавання динамічних сабок з інпутів форми.
-
-
-header('Location: success.php?pixel='.$_POST['pixel']);  -  
-код для перекидання на сторінку Дякую.
